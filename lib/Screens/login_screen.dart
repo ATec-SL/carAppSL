13946ce6a9ebac323d944fd76b68a82302468d72
AkashJ <akash.jayaweera@kohls.com>
2020-01-16 17:11:54 +0530

    Implement login screen UI design

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  _submit() {
+
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      print(_email);
+      print(_password);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(  //Ceneter everything
+
+        child: Column(  //Create a column child in cscaffold
+          mainAxisAlignment: MainAxisAlignment.center,  //To center the title
+          crossAxisAlignment: CrossAxisAlignment.center,  //To center the title'
+          children: <Widget>[    //Inside this widget we do all the login screen design
+
+            CircleAvatar(   //Add app logo
+              radius: 50.0,
+              backgroundColor: Colors.grey,
+              backgroundImage: AssetImage('assets/images/logotemp.png')
+            ),
+
+            Text('ZoneGram',
+              style: TextStyle(
+                fontSize: 50.0,   //Add style to text******
+                fontFamily: 'Pacifico'  //Need to add this font type to pubspec.yaml --fonts
+            ),
+            ),
+
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+
+                  Padding(   //Email
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),   //Add padding around text field
+                    child: TextFormField(    // Input label email
+                      decoration: InputDecoration(labelText: 'Email'),
+                      validator: (input) => !input.contains('@') ? 'Please enter a valid email': null,
+                      onSaved: (input) => _email = input,
+                    ),
+                  ),
+
+                  Padding( //Password
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),   //Add padding around text field
+                    child: TextFormField(    // Input label email
+                      decoration: InputDecoration(labelText: 'Password'),
+                      validator: (input) => input.length < 6 ? 'Must be atleast 6 characters' : null,
+                      onSaved: (input) => _password = input,
+                      obscureText: true,  //character hidden
+                    ),
+                  ),
+
+                  SizedBox(height: 20.0),
+                  Container(   //Login button container
+                    width: 250.0,
+                    child: FlatButton(    // Login button
+                      onPressed: _submit,
+                      color: Colors.lightBlue,
+                      padding: EdgeInsets.all(10.0),
+                      child: Text('Login',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20.0),
+
+                  Container(   //Sign up button container
+                    width: 250.0,
+                    child: FlatButton(    // Login button
+                      onPressed: _submit,
+                      color: Colors.lightBlue,
+                      padding: EdgeInsets.all(10.0),
+                      child: Text('Login',
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                    ),
+                  )
+
+
+                ],
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
